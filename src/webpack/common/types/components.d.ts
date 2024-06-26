e3e1555a368a559052589a9aa73ebbaebebd6b97
sunnie <78964224+sunnniee@users.noreply.github.com>
2024-06-26 12:11:32 +0300

    this is easier than resolving merge conflicts

--- src/webpack/common/types/components.d.ts
+++ src/webpack/common/types/components.d.ts
@@ -458,3 +458,9 @@ export type Avatar = ComponentType<PropsWithChildren<{
 type FocusLock = ComponentType<PropsWithChildren<{
     containerRef: RefObject<HTMLElement>;
 }>>;
+
+export type Dots = ComponentType<PropsWithChildren<{
+    dotRadius: number;
+    themed?: boolean;
+    className?: string;
+}>>;
